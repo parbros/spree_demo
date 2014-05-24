6f69548ac2d7f9bf9ff13fea669d58e1b6ab3948
Jorge Pardiñas <jparbros@gmail.com>
2014-05-24 00:55:10 -0500

    home page completed

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,6 +2,6 @@ Spree::HomeController.class_eval do
   def index
     @slides = Spree::Slide.published.order('position ASC')
     @popular_products = Spree::Product.popular_products(8)
-    @recent_products = Spree::Product.recent_product(4)
+    @recent_products = Spree::Product.recent_product(8)
   end
 end
