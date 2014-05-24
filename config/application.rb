76565a5db7d6c501432f5cbd31ea4a78db292d5b
Jorge Pardiñas <jparbros@gmail.com>
2014-05-24 14:35:12 -0500

    no initialize in precompile

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module SpreeDemo
 
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
