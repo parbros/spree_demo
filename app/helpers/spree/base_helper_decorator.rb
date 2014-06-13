dc2fccf1f62d87c6f6ff09a918ac2afbb66c7d37
Jorge Pardiñas <jparbros@gmail.com>
2014-06-13 01:11:53 -0500

    new images

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -6,7 +6,7 @@ Spree::BaseHelper.class_eval do
       root_taxon.children.map do |taxon|
         css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
         content_tag :li, class: css_class do
-          link_to("<i class=\"fa fa-circle\"></i> #{taxon.name}".html_safe, seo_url(taxon)) +
+          link_to("<i class=\"fa fa-angle-double-right\"></i> #{taxon.name}".html_safe, seo_url(taxon)) +
           taxons_tree(taxon, current_taxon, max_level - 1)
         end
       end.join("\n").html_safe
